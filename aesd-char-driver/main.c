97861d7ffdb9dc3746a970b2ef1a947092481593
Comert Sagiroglu <comerts@gmail.com>
2025-02-28 14:09:29 +0300

    write f_pos update disabled

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -222,7 +222,7 @@ ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count, loff
     }
 
     retval = count;
-    *f_pos += count;
+    //*f_pos += count;
 
 out:
     mutex_unlock(&dev->lock);
